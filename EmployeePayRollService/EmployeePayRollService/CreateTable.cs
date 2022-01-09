3399b189718bfcc5c1ccf27ec45dcdfc8a5a9aa7
rajashri03 <telvekar.rajashri@gmail.com>
2022-01-09 13:10:23 +0530

    [Rajashri]Add:Create table-Employee payroll

---
+++ EmployeePayRollService/EmployeePayRollService/CreateTable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRollService
+{
+    public class CreateTable
+    {
+        /// <summary>
+        /// Create Database-PayrollService
+        /// </summary>
+        public static void Create_Database()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=AD-PC\SQLEXPRESS; Initial Catalog =master; Integrated Security = True;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Create database payroll_service;", con);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Database created successfully!");
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating database:" + e.Message + "\t");
+            }
+        }
+        /// <summary>
+        /// Created table using sqlcommand in payrollservice database
+        /// </summary>
+        public static void CreateTables()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=AD-PC\SQLEXPRESS; Initial Catalog =payroll_service; Integrated Security = True;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Create table Employee_Payroll(id int identity(1,1) primary key,Name varchar(200),Salary varchar(200),Start_date DATE);", con);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Employee Payroll table has been  created successfully!");
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating table:" + e.Message + "\t");
+            }
+        }
+    }
+}
--- EmployeePayRollService/EmployeePayRollService/Program.cs
+++ EmployeePayRollService/EmployeePayRollService/Program.cs
@@ -2,4 +2,20 @@
 using EmployeePayRollService;
 
 Console.WriteLine("****************Employee Payroll Service***********************");
-CreateDatabase.Create_Database();
+bool options = true;
+while(options)
+{
+    Console.WriteLine("Select Option:\n1)Create Database\n2)Create Table");
+    int op = Convert.ToInt16(Console.ReadLine());
+    switch(op)
+    {
+        case 1:
+            CreateTable.Create_Database();
+            break;
+        case 2:
+            CreateTable.CreateTables();
+            break;
+    }
+}
+
+
